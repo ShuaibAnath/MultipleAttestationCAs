8d1ab8bf11b55b1b22e2cc976cd5433c080621c9
ShuaibAnath <80897486+ShuaibAnath@users.noreply.github.com>
2025-04-03 09:56:21 +0200

    Update generate_ak_cert.sh

--- AttestationCA/generate_ak_cert.sh
+++ AttestationCA/generate_ak_cert.sh
@@ -3,4 +3,4 @@
 AK_CSR=$1
 AK_CERT=$2
 # Sign the certificate created previously with the same private key: 
-openssl x509 -req -days 365 -in $AK_CSR -signkey CAkeys/ca1_private_key.pem -out $AK_CERT
+openssl x509 -req -days 365 -in $AK_CSR -signkey CAkeys/ca_private_key.pem -out $AK_CERT
